1d99497c4264b0ae05970dd6188aa9dad1a8bde3
Krym <gumimash@gmail.com>
2024-02-10 17:05:44 +0200

    added HTTP functionality

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -3,10 +3,11 @@ import { CommonModule } from "@angular/common";
 import { ActivatedRoute } from "@angular/router";
 import { HousingService } from "../housing.service";
 import { HousingLocation } from "../housing-location";
+import { FormControl, FormGroup, ReactiveFormsModule } from "@angular/forms";
 @Component({
   selector: "app-details",
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   template: `
     <article>
       <img class="listing-photo" [src]="housingLocation?.photo" />
@@ -27,7 +28,18 @@ import { HousingLocation } from "../housing-location";
         </ul>
         <section class="listing-apply">
           <h2 class="section-heading">Apply to live here</h2>
-          <button class="primary">Apply now</button>
+          <form [formGroup]="applyForm" (submit)="submitApplication()">
+            <label for="first-name">First Name</label>
+            <input id="first-name" type="text" formControlName="firstName" />
+
+            <label for="last-name">last Name</label>
+            <input id="last-name" type="text" formControlName="lastName" />
+
+            <label for="email">email</label>
+            <input id="email" type="email" formControlName="email" />
+
+            <button type="submit" class="primary">Apply now</button>
+          </form>
         </section>
       </section>
     </article>
@@ -38,12 +50,25 @@ export class DetailsComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
   housingService = inject(HousingService);
   housingLocation: HousingLocation | undefined;
-
+  applyForm = new FormGroup({
+    firstName: new FormControl(""),
+    lastName: new FormControl(""),
+    email: new FormControl(""),
+  });
   constructor() {
     const housingLocationId = Number(this.route.snapshot.params["id"]);
-    this.housingLocation =
-      this.housingService.getHousingLocationById(housingLocationId);
-    console.log(housingLocationId);
-    console.log(this.housingLocation?.id);
+    this.housingService
+      .getHousingLocationById(housingLocationId)
+      .then((housingLocation) => {
+        this.housingLocation = housingLocation;
+      });
+  }
+
+  submitApplication() {
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? "",
+      this.applyForm.value.lastName ?? "",
+      this.applyForm.value.email ?? ""
+    );
   }
 }
